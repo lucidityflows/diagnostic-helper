bf94dfda5774e8665ef15b2a5a5c391e9dfabdca
Robert Torres <rtorres5@stedards.edu>
2020-05-15 21:47:34 -0500

    diagnostic_helper.py and diagnostic_utils.py created helper functions created to run timed diagnostic tests that measure performance

---
+++ diagnostic_helper.py
@@ -0,0 +1,15 @@
+# File Name: gateway_diagnostics.py
+# Purpose: Represents the Gateway Diagnostics component.
+#          To create a local socket server for the Gateway Controller. Depending on the message received from the
+#          Gateway Controller, this program will get the requested data, compile it into a dictionary, and send it
+#          back to the Gateway Controller as a JSON format.
+
+import diagnostic_utils
+
+
+def main():
+
+    return
+
+# Call main function
+main()
